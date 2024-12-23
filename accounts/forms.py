96ffed080bd0ffa882acd9c9d18ce67efb1e7502
snezhanaustimova <59029577+snezhanaustimova@users.noreply.github.com>
2024-12-24 01:22:18 +0300

    Add files via upload

---
+++ accounts/forms.py
@@ -0,0 +1,95 @@
+from django import forms
+from django.conf import settings
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.db import transaction
+
+from .models import User, BankAccountType, UserBankAccount, UserAddress
+from .constants import GENDER_CHOICE
+from .security_container import validate_input
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label="Email",
+                    widget=forms.EmailInput(attrs={'class': 'form-input'}), validators=[validate_input])
+    password = forms.CharField(label="Password",
+                    widget=forms.PasswordInput(attrs={'class': 'form-input'}), validators=[validate_input])
+
+
+class UserAddressForm(forms.ModelForm):
+
+    class Meta:
+        model = UserAddress
+        fields = [
+            'street_address',
+            'city',
+            'postal_code',
+            'country'
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': (
+                    'appearance-none block w-full bg-gray-200 '
+                    'text-gray-700 border border-gray-200 rounded '
+                    'py-3 px-4 leading-tight focus:outline-none '
+                    'focus:bg-white focus:border-gray-500'
+                )
+            })
+
+
+class UserRegistrationForm(UserCreationForm):
+    account_type = forms.ModelChoiceField(
+        queryset=BankAccountType.objects.all()
+    )
+    gender = forms.ChoiceField(choices=GENDER_CHOICE)
+    birth_date = forms.DateField(widget=forms.DateInput(attrs={'type':'date'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'autofocus':'on'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'autofocus':'off'}), validators=[validate_input])
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'password1',
+            'password2',
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': (
+                    'appearance-none block w-full bg-gray-200 '
+                    'text-gray-700 border border-gray-200 '
+                    'rounded py-3 px-4 leading-tight '
+                    'focus:outline-none focus:bg-white '
+                    'focus:border-gray-500'
+                )
+            })
+
+    @transaction.atomic
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+            account_type = self.cleaned_data.get('account_type')
+            gender = self.cleaned_data.get('gender')
+            birth_date = self.cleaned_data.get('birth_date')
+
+            UserBankAccount.objects.create(
+                user=user,
+                gender=gender,
+                birth_date=birth_date,
+                account_type=account_type,
+                account_no=(
+                    user.id +
+                    settings.ACCOUNT_NUMBER_START_FROM
+                )
+            )
+        return user
---
+++ accounts/security_container.py
@@ -0,0 +1,28 @@
+import requests
+import re
+from django import forms
+
+
+# Функция для проверки ввода пользователя
+def validate_input(user_input):
+    # Проверка на наличие подозрительных символов
+    if re.search(r'(\'|;|\")', user_input):
+        raise forms.ValidationError("Suspicious characters found")
+
+    # Проверка на наличие SQL-инъекций
+    compile = re.compile(r'(SELECT|INSERT|UPDATE|DELETE|WHERE|HAVING|ORDER|BY|LIMIT)')
+    if re.match(compile, user_input.upper()):
+        raise forms.ValidationError("Suspicious SQL queries found")
+
+    # Проверка длины ввода
+    if len(user_input) > 35:
+        raise forms.ValidationError("The maximum input length has been exceeded")
+
+    # Проверка формата ввода
+    compile = re.compile(r'[\w\d\-_\.\@\#]+$')
+    if not re.match(compile, user_input):
+        raise forms.ValidationError("The input contains invalid characters")
+
+    # Проверка на словарные атаки
+    if user_input.lower() in ["admin", "password", "root", "default", "guest"]:
+        raise forms.ValidationError("The input is a dictionary word")
